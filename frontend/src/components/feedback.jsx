bc47e55bb569eab99bd32d01c3874ca552a75ca7
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-04-01 12:03:25 +0800

    modified feedback section

--- frontend/src/components/feedback.jsx
+++ frontend/src/components/feedback.jsx
@@ -1,8 +1,15 @@
-
+import { testimonials } from "../constants/index.jsx"
 
 const feedbackComments = () => {
   return (
-    <div>feedbackComments</div>
+    <div className="mt-20 tracking-wide">
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">Feedback from Users!</h2>
+      <div className="flex flex-wrap justify-center">
+      {testimonials.map((testimonials, index) => (
+        
+      ))}
+      </div>
+    </div>
   )
 }
 
